bd63f62fc8d8859fb15ef1947b77cc3ac62c2a15
RDonii <90790952+RDonii@users.noreply.github.com>
2022-04-27 12:54:29 +0500

    inline kwrd added

--- handlers/users/dasturlash.py
+++ handlers/users/dasturlash.py
@@ -3,7 +3,7 @@ from aiogram.dispatcher.filters import Text
 
 from loader import dp
 from states.dasturlash import Dasturlash 
-from keyboards.default.lessonsKeyboards import lessons
+from keyboards.inline.productInlinekeyboards import productInline
 from keyboards.default.pythonMenuKeyboards import pythonMenu
 
 @dp.message_handler(Text(equals='Python darslari'), state=Dasturlash.dasturlashMenu)
@@ -13,15 +13,15 @@ async def python_menu(message: types.Message):
 
 @dp.message_handler(Text(equals='Delphi'), state=Dasturlash.dasturlashMenu)
 async def delphi(message: types.Message):
-    await message.answer('Yangi bilimlardan bahra oling!', reply_markup=lessons)
+    await message.answer('Yangi bilimlardan bahra oling!', reply_markup=productInline)
     await Dasturlash.delphi.set()
 
 @dp.message_handler(Text(equals='Golang'), state=Dasturlash.dasturlashMenu)
 async def golang(message: types.Message):
-    await message.answer('Yangi bilimlardan bahra oling!', reply_markup=lessons)
+    await message.answer('Yangi bilimlardan bahra oling!', reply_markup=productInline)
     await Dasturlash.golang.set()
 
 @dp.message_handler(Text(equals='JavaScript'), state=Dasturlash.dasturlashMenu)
 async def javascript(message: types.Message):
-    await message.answer('Yangi bilimlardan bahra oling!', reply_markup=lessons)
+    await message.answer('Yangi bilimlardan bahra oling!', reply_markup=productInline)
     await Dasturlash.javaScript.set()
