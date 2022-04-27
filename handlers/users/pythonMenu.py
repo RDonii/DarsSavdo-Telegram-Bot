ca4731dc677fdf571395445c465924dc7efd7a63
RDonii <90790952+RDonii@users.noreply.github.com>
2022-04-27 12:35:42 +0500

    inline keybrs added

--- handlers/users/pythonMenu.py
+++ handlers/users/pythonMenu.py
@@ -8,15 +8,15 @@ from keyboards.inline.productInlinekeyboards import productInline
 
 @dp.message_handler(Text(equals="Python Backend"), state=Dasturlash.python)
 async def python_backend(message: types.Message):
-    await message.answer('Yangi bilimlardan bahra oling!', reply_markup=productInline)
+    await message.answer('Python Backend Darslari', reply_markup=productInline)
     await Dasturlash.pythonBackend.set()
 
 @dp.message_handler(Text(equals="Python Sun`iy Ong"), state=Dasturlash.python)
 async def python_AI(message: types.Message):
-    await message.answer('Yangi bilimlardan bahra oling!', reply_markup=productInline)
+    await message.answer('Python Sun`iy Ong Darslari', reply_markup=productInline)
     await Dasturlash.pythonAI.set()
 
 @dp.message_handler(Text(equals="Python Ma`lumotshunos"), state=Dasturlash.python)
 async def python_backend(message: types.Message):
-    await message.answer('Yangi bilimlardan bahra oling!', reply_markup=productInline)
+    await message.answer('Python Ma`lumotshunos Kursi', reply_markup=productInline)
     await Dasturlash.pythonDS.set()
